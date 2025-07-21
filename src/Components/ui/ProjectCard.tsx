a51e60cbb4f6ddb89f1c0e3cbd21c37b8572d2e9
zairz7018 <zairz7018@gmail.com>
2025-07-21 20:23:16 +0100

    chore: update project with minor changes and improvements

--- src/Components/ui/ProjectCard.tsx
+++ src/Components/ui/ProjectCard.tsx
@@ -36,7 +36,7 @@ export function ProjectCard({ title, description, image, link, tags }: ProjectCa
             </a>
             {/* GitHub Link */}
             <a
-              href={`https://github.com/deepakmodi/${title.toLowerCase()}`}
+              href={`https://github.com/zairz7018/${title.toLowerCase()}`}
               target="_blank"
               rel="noopener noreferrer"
               className="p-3 bg-white/20 rounded-full hover:bg-gray-900 hover:scale-110 transition-transform duration-300 ease-in-out shadow-lg"
